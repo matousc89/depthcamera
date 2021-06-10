0838dc5f61365a40a1b5e8317705d36ffbd8ccdb
Your Name <you@example.com>
2021-06-10 11:48:28 +0200

    first example

--- main.py
+++ main.py
@@ -7,6 +7,8 @@
 
 # First import the library
 import pyrealsense2 as rs
+import numpy as np
+import cv2
 
 try:
     # Create a context object. This object owns the handles to all connected realsense devices
@@ -14,38 +16,55 @@ try:
 
     # Configure streams
     config = rs.config()
+    config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
     config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
 
     # Start streaming
     pipeline.start(config)
 
+
+
+    # depth_to_disparity = rs.disparity_transform(True)
+    # disparity_to_depth = rs.disparity_transform(False)
+    # dec_filter = rs.decimation_filter()
+    # temp_filter = rs.temporal_filter()
+    # spat_filter = rs.spatial_filter()
+
     while True:
         # This call waits until a new coherent set of frames is available on a device
         # Calls to get_frame_data(...) and get_frame_timestamp(...) on a device will return stable values until wait_for_frames(...) is called
         frames = pipeline.wait_for_frames()
-        depth = frames.get_depth_frame()
-        if not depth: continue
+        depth_frame = frames.get_depth_frame()
+        color_frame = frames.get_color_frame()
 
-        # Print a simple text-based representation of the image, by breaking it into 10x20 pixel regions and approximating the coverage of pixels within one meter
-        coverage = [0] * 64
-        for y in range(480):
-            for x in range(640):
-                dist = depth.get_distance(x, y)
-                if 0 < dist and dist < 1:
-                    coverage[x // 10] += 1
+        if not depth_frame: continue
 
-            if y % 20 == 19:
-                line = ""
-                for c in coverage:
-                    line += " .:nhBXWW"[c // 25]
-                coverage = [0] * 64
-                print(line)
+
+
+        # depth_frame = depth_to_disparity.process(depth_frame)
+        # depth_frame = dec_filter.process(depth_frame)
+        # depth_frame = temp_filter.process(depth_frame)
+        # depth_frame = spat_filter.process(depth_frame)
+        # depth_frame = disparity_to_depth.process(depth_frame)
+        # depth_frame = depth_frame.as_depth_frame()
+
+
+        depth_data = depth_frame.as_frame().get_data()
+        rgb_data = color_frame.as_frame().get_data()
+
+        rgb_image = np.asanyarray(rgb_data)
+        depth_image = np.asanyarray(depth_data)
+        depth_image = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
+
+
+        cv2.imshow('RGB', rgb_image)
+        cv2.imshow('depth', depth_image)
+
+
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            break
+
     exit(0)
-# except rs.error as e:
-#    # Method calls agaisnt librealsense objects may throw exceptions of type pylibrs.error
-#    print("pylibrs.error was thrown when calling %s(%s):\n", % (e.get_failed_function(), e.get_failed_args()))
-#    print("    %s\n", e.what())
-#    exit(1)
 except Exception as e:
     print(e)
     pass
